61300647543b54211e539840ec4f38e97e87bb7f
gourav-sharma1857 <whogourav@gmail.com>
2026-06-13 17:17:59 -0500

    Fixed a logic error

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CareerOS/'
+  base: '/CareerOS/',
+   build: {
+    chunkSizeWarningLimit: 1000, // Increases the limit to 1000kB
+  }
 })
